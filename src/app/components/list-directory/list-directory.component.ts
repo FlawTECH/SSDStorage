17983375ff1a392ce67f3194006c2ac45ee52d51
ma-agent <zeyd.salef@gmail.com>
2019-01-13 15:03:45 +0100

    updated listdirectory front end

--- src/app/components/list-directory/list-directory.component.ts
+++ src/app/components/list-directory/list-directory.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FileService } from '../../services/file.service';
 import { File } from '../../class/file';
 import * as jwtDecode from 'jwt-decode';
+import { tokenKey } from '@angular/core/src/view';
 
 
 @Component({
@@ -10,24 +11,35 @@ import * as jwtDecode from 'jwt-decode';
   styleUrls: ['./list-directory.component.scss']
 })
 export class ListDirectoryComponent implements OnInit {
-
   private userFileList: File[] = [];
   private userFolderList : File[] = [];
-  
+  private currentPath:String;
   private token:any;
+
   constructor(private fileService:FileService) {
     this.token = jwtDecode(localStorage.getItem("AuthToken"));
+    
   }
+  
 
   ngOnInit() {
     
+    
+
     this.fileService.getFile(this.token.fullname).subscribe(
       (res) =>{
         //console.log(res);
         res.forEach(element => {
-          this.userFileList.push(File.fromJSON(element.file))
+          console.log(element.file.type);
+          
+          if(element.file.type ==="f"){
+            this.userFileList.push(File.fromJSON(element.file));
+          }else{
+            this.userFolderList.push(File.fromJSON(element.file));
+          }
         });
-        console.log(this.userFileList)
+        console.log(res)
+        this.currentPath = this.userFileList[0].path+"/";
       }
     )
   }
